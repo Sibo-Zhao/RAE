531892b7309fdf8b8f02c8601fc4138bfb4d2cba
sunoneeye1 <sunoneeye@gmail.com>
2024-11-18 21:38:13 +0800

    Initial commit

---
+++ main/evaluator/__init__.py
@@ -0,0 +1,7 @@
+try:
+    from evaluator.cpp.evaluate_foldout import eval_score_matrix_foldout
+    print("eval_score_matrix_foldout with cpp")
+except:
+    from evaluator.python.evaluate_foldout import eval_score_matrix_foldout
+    print("eval_score_matrix_foldout with python")
+    
